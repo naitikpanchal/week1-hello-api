87b061b5d97d28666bdf11831218f7a93518cd4f
Naitik Panchal <naitikpanchal25@gmail.com>
2025-09-09 17:05:13 -0400

    disabled type checking for mock test object

--- src/__test__/health.test.ts
+++ src/__test__/health.test.ts
@@ -5,7 +5,7 @@ import { Request, Response } from 'express';
 describe('Health controller Check', () => {
     it('should return status ok and uptime', () => {
         // const req: Request = {};
-        const res: Response = {
+        const res: any = {
             json: (response: HealthResponse) => {
                 expect(response.status).toBe('ok');
                 expect(response.uptime).toBeTypeOf('number');
